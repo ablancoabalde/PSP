c5e6d32f6ebf2c7ceaaaece167451cb82e811fa1
Alberto <ablancoabalde@danielcastelao.org>
2019-03-26 14:17:39 +0100

    Añadidos proyectos

---
+++ EjemploSocketDatagramCliente.java/src/ejemplosocketdatagramcliente/java/EjemploSocketDatagramClienteJava.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosocketdatagramcliente.java;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+/**
+ *
+ * @author Alberto
+ */
+public class EjemploSocketDatagramClienteJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       try{
+            System.out.println("Creando socket datagram");
+            DatagramSocket socketUDP=new DatagramSocket(5556);
+
+            InetAddress addr2=InetAddress.getByName("localhost");
+            
+            String mensaje="5";
+            DatagramPacket datagrama=new DatagramPacket(mensaje.getBytes(),mensaje.getBytes().length,addr2,5555);
+            socketUDP.send(datagrama);
+            System.out.println("numero enviado");
+            
+            mensaje="7";
+            datagrama=new DatagramPacket(mensaje.getBytes(),mensaje.getBytes().length,addr2,5555);
+            socketUDP.send(datagrama);
+            System.out.println("numero enviado");
+           
+            System.out.println("Recibiendo número");
+            byte[] recibir=new byte[25];
+            DatagramPacket respuesta=new DatagramPacket(recibir,25);
+            socketUDP.receive(respuesta);
+            System.out.println("Número recibido: "+new String(recibir));
+            
+            System.out.println("Cerrando el socket datagrama");
+
+            socketUDP.close();
+
+            System.out.println("Terminado");
+        }catch(IOException e){
+            e.printStackTrace();
+	}
+    }
+    
+}
